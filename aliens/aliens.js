691edf4b7314cef58b974576c59f42cd44680f2a
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2016-11-02 08:54:38 -1000

    update

---
+++ aliens/aliens.js
@@ -0,0 +1,16 @@
+var blue = 1500;
+var red = 2500;
+var green = 6000;
+var aliens = 10000;
+
+if (blue+green+red == aliens) {
+ if (green > 5000) {
+  console.log("ugh, why is zak so ugly");
+  }
+ if (green + red > 8000){
+  console.log("zak sucks at soccer");
+  }
+ if( red + blue < 3000){
+  console.log("zakattack808");
+}
+} 
